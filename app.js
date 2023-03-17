c82490a401525fff4292d29b8965b5fc38e3437e
utsavgohel-simform <utsav@simformsolutions.com>
2023-03-17 17:45:28 +0530

    feat: belongsto,hasmany,manytomany routes add

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.get("/finder", userController.finderData);
 app.get("/validation", userController.validationController);
 app.get("/rawQuery", userController.rawQuery);
 app.get("/oneToOne", userController.oneToOne);
+app.get("/belongTo", userController.belongTo);
+app.get("/hasMany", userController.hasMany);
+app.get("/ManyToMany", userController.ManyToMany);
 
 app.listen(3000, () => {
   console.log("Server Running");
